a9e90e144d8feca7c8c4aed0165c1ad43e160caa
Priyanshu saini <142074515+priyanshusaini872@users.noreply.github.com>
2024-03-07 21:25:13 +0530

    Add files via upload

---
+++ gallery for event/index.js
@@ -0,0 +1,14 @@
+// $("#myImg").css("visibility","hidden");
+
+// window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+//     $("#myImg").css("visibility","visible");
+$("#myImg").addClass("slideUp");
+//   }
+// }
+
+$("h2").addClass("slideLeft");
+$("h1").addClass("slideLeft");
+$("p").addClass("slideRight");
